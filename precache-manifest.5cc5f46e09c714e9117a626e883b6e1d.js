36a544d393d513ede653a2ae3a36d8b22e90057d
Trevor Marlowe <marlowetrevor@hotmail.com>
2019-07-06 23:58:58 -0600

    Updates

---
+++ precache-manifest.5cc5f46e09c714e9117a626e883b6e1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "143eb19aa518302daf32",
+    "url": "/robofriends/static/js/runtime~main.fb41d28d.js"
+  },
+  {
+    "revision": "77d77dbf4d7f8b99ee66",
+    "url": "/robofriends/static/js/main.8c00d693.chunk.js"
+  },
+  {
+    "revision": "7c37e23d22d11893df95",
+    "url": "/robofriends/static/js/2.5fc7425f.chunk.js"
+  },
+  {
+    "revision": "77d77dbf4d7f8b99ee66",
+    "url": "/robofriends/static/css/main.a2d63d22.chunk.css"
+  },
+  {
+    "revision": "7c37e23d22d11893df95",
+    "url": "/robofriends/static/css/2.fe8c271d.chunk.css"
+  },
+  {
+    "revision": "dac49569358042edfe38bd22359d32f2",
+    "url": "/robofriends/index.html"
+  }
+];
